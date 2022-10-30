26504f52cf86e6f9c1fa748e34a3d0078240dedd
dhiastiii <dhiastiputri.jayanti@gmail.com>
2022-10-30 19:14:40 +0700

    membuat Controller

---
+++ src/test/java/TakeHome1/takehome_1/myController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TakeHome1.takehome_1;
+
+/**
+ *
+ * @author HAHAHA
+ */
+public class myController {
+    
+}
